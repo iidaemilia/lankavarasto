d310e600d4255ca5cd029b8da0e7b6263ea9cb37
Iida Lehtonen <iidalehtonen@Iida-MacBook-Pro.local>
2025-03-14 15:33:32 +0200

    Javadoc dokumentaatio lisätty

--- lankavarasto/Lanka.java
+++ lankavarasto/Lanka.java
@@ -1,3 +1,9 @@
+/**
+ * @author Iida
+ * @version 1.0
+ */
+
+package lankavarasto;
 /**Olioluokka Lanka */
 public class Lanka {
     private String nimi;
@@ -7,11 +13,20 @@ public class Lanka {
     private boolean sukkalanka;
     private boolean varattu;
 
-    /**parametriton alustaja*/
+    /**
+     * parametriton alustaja uuden lankaolion luomista varten.
+     */
     public Lanka(){
     }
 
-    /**parametrillinen alustaja langalle */
+    /**parametrillinen alustaja luo uuden lankaolion.
+     * @param nimi kertoo langan nimen.
+     * @param maara kertoo langan määrän grammoina.
+     * @param juoksevuus kertoo langan juoksevuuden eli metrimäärän per 100g.
+     * @param vari kertoo langan värin.
+     * @param sukkalanka kertoo, onko kyseessä sukkiin kelpaava (vahvistettu) lanka.
+     * @param varattu kertoo, onko lanka varattu johonkin käsityöprojektiin.
+     */
     public Lanka(String nimi, double maara, double juoksevuus, String vari, boolean sukkalanka){
         this.nimi=nimi;
         this.maara=maara;
@@ -20,90 +35,136 @@ public class Lanka {
         this.sukkalanka=sukkalanka;
     }
 
-    /**Set-metodi nimen asettamiselle*/
+    /**
+     * Asettaa nimen.
+     * @param nimi uusi nimi lankaoliolle.
+    */
     public void setNimi(String nimi){
         this.nimi=nimi;
     }
     
-    /**Set-metodi langan määrän asettamiselle */
+    /**
+     * Asettaa määrän.
+     * @param maara uusi määrä lankaoliolle.
+    */
     public void setMaara(double maara){
         this.maara=maara;
     }
 
-    /**Set-metodi langan juoksevuuden asettamiselle */
+    /**
+     * Asettaa juoksevuuden.
+     * @param juoksevuus uusi juoksevuus lankaoliolle.
+     */
     public void setJuoksevuus(double juoksevuus){
         this.juoksevuus=juoksevuus;
     }
 
-    /**Set-metodi langan värin asettamiselle */
+    /**
+     * Asettaa värin.
+     * @param vari uusi väri lankaoliolle.
+    */
     public void setVari(String vari){
         this.vari=vari;
     }
 
-    /**Set-metodi sen määrittämiselle onko lanka sukkalankaa vai ei */
+    /**
+     * Asettaa arvon sukkalangalle
+     * @param sukkalanka onko sukkalanka vai ei.
+    */
     public void setSukkalanka(boolean sukkalanka){
         this.sukkalanka=sukkalanka;
     }
 
-    /**Set-metodi sen määrittämiseksi, onko lanka varattu jo johonkin käsityöprojektiin */
+    /**
+     * Asettaa arvon siitä, onko lanka varattu toiseen projektiin.
+     * @param varattu kertoo, onko varattu vai ei.
+    */
     public void setVarattu(boolean varattu){
         this.varattu=varattu;
     }
 
-    /**Seuraavaksi määritellään arvon palauttavat get-metodit */
-    /**Get-metodi langan nimen palauttamiselle */
+    /**
+     * Palauttaa nimen.
+     * @return langan nimi.
+    */
     public String getNimi(){
         return nimi;
     }
 
-    /**Get-metodi langan määrän palauttamiseksi */
+    /**
+     * Palauttaa määrän.
+     * @return langan määrä.
+    */
     public double getMaara(){
         return maara;
     }
 
-    /**Get-metodi langan juoksevuuden palauttamiselle */
+    /**
+     * Palauttaa juoksevuuden.
+     * @return langan juoksevuus.
+    */
     public double getJuoksevuus(){
         return juoksevuus;
     }
 
-    /**Get-metodi värin palauttamiselle */
+    /**
+     * Palauttaa langan värin.
+     * @return langan väri.
+     */
     public String getVari(){
         return vari;
     }
 
-    /**Get-metodi sen palauttamiselle, onko lanka sukkalankaa vai ei */
+    /**
+     * Palauttaa tiedon, onko sukkalanka vai ei.
+     * @return onko sukkalanka.
+     */
     public boolean getSukkalanka(){
         return sukkalanka;
     }
 
-    /**Get-metodin sen palauttamiselle, onko lanka jo varattu projektiin vai ei */
+    /**
+     * Palauttaa tiedon, onko lanka varattu toiseen projektiin vai ei.
+     * @return onko varattu.
+     */
     public boolean getVarattu(){
         return varattu;
     }
 
     /**TÄSSÄ EKA 30 MIN = loin olioluokan Lanka, muuttujat, konstruktorit sekä getterit ja setterit */
 
-    /**Metodeja ohjelman toimintaan liittyen */
-    /**Lisää langan määrää -metodi */
+    /**
+     * Lisää lankaa langan määrään.
+     * @param maara määrä, johon lankaa lisätään.
+     * @param lisattavaMaara määrä, joka lankaa halutaan lisätä.
+     * @param uusiMaara uusi määrä, johon haluttu määrä on lisätty.
+     * @return tieto, että lanka on lisätty onnistuneesti.
+     */
     public String lisaaLankaa(double lisattavaMaara){
         double uusiMaara=maara+lisattavaMaara;
         maara=uusiMaara;
         return "Lanka lisätty";
     }
-    /**Metodi, joka vähentää langan määrää */
+    /**
+     * Vähentää langan määrää. 
+     * Tarkistaa, onko lankaa mahdollista vähentää haluttu määrä.
+     * @param vahennettavaMaara lankavaraston langasta vähennettävä määrä
+     * Jos lankaa voidaan vähentää
+     * @return true
+     * Jos lankaa ei voida vähentää
+     * @return false
+     * Jos lanka vähennyksen yhteydessä loppuu
+     * @return true ja tieto, että lanka loppui.
+    */
     public boolean vahennaLankaa(double vahennettavaMaara) {
-        /**Tarkistetaan, onko lankaa liian vähän vähennyksen tekemiseen, jos on, palautetaan false */
         if (vahennettavaMaara > maara) {
             System.out.println("Virhe: lankaa on liian vähän.");
             return false;
         }
-        /**Tehdään vähennys, jos se onnistuu */
         maara -= vahennettavaMaara;
-        /**Jos kaikki lanka käytetään, ilmoitetaan erikseen, että lanka loppui nyt */
         if (maara == 0) {
             System.out.println("Lanka loppui nyt.");
         }
-        /**palautetaan true, kun vähennys onnistui */
         return true;
     }
     /**TÄSSÄ ENSIMMÄISEN TUNNIN TYÖ - lisätty langan lisäykseen ja vähentämiseen metodit */
@@ -111,3 +172,7 @@ public static void main(String[] args) {
     System.out.println("moi");
 }
 }
+
+/**
+ * Käytetty 40 min Javadoc-dokumentaatioon olioluokalle 
+ */
